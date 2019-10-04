0fd267d6204487ec7f957b9bf5ef7473932f51b7
youvovk <wer3wollf@gmail.com>
2019-10-04 13:12:02 +0300

    Updates

---
+++ precache-manifest.8734e29e7daa621fccfbf1c256ddee6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fd4ce254f75a04142cdc55a5e17529",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "121bd725d9c5960c4298",
+    "url": "/react_dynamic-list-of-todos/static/css/main.efbf4300.chunk.css"
+  },
+  {
+    "revision": "10181f91705da802c2f2",
+    "url": "/react_dynamic-list-of-todos/static/js/2.51d9639a.chunk.js"
+  },
+  {
+    "revision": "121bd725d9c5960c4298",
+    "url": "/react_dynamic-list-of-todos/static/js/main.d42a58bc.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  },
+  {
+    "revision": "1fdd2ebf0be1f6924f7267abfbd2a0cf",
+    "url": "/react_dynamic-list-of-todos/static/media/ch.1fdd2ebf.svg"
+  },
+  {
+    "revision": "231a7dcbabe4604443041df252673f74",
+    "url": "/react_dynamic-list-of-todos/static/media/folder.231a7dcb.svg"
+  },
+  {
+    "revision": "63cb4921cba1eb35818db9277cb3a18d",
+    "url": "/react_dynamic-list-of-todos/static/media/id-card.63cb4921.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_redux-list-of-todos/precache-manifest.ef6f636eb3b3cdb10ebc22b4adaac495.js"
+  "/react_dynamic-list-of-todos/precache-manifest.8734e29e7daa621fccfbf1c256ddee6d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_redux-list-of-todos/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_dynamic-list-of-todos/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
